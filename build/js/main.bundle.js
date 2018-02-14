48d2c30f4ae4aa415b30ac9315d906c8592d4742
DESKTOP-G40P2G4\Lucas Lima <lucas.lima@enext.com.br>
2018-02-14 00:47:59 -0200

    carousel sync

--- build/js/main.bundle.js
+++ build/js/main.bundle.js
@@ -1,2 +1,25 @@
-"use strict";
+'use strict';
+
+var carousel = function carousel() {
+   console.log("loles");
+   $('.slider-for').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      fade: true,
+      asNavFor: '.slider-nav'
+   });
+   $('.slider-nav').slick({
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      asNavFor: '.slider-for',
+      dots: true,
+      centerMode: true,
+      focusOnSelect: true
+   });
+};
+
+$(document).ready(function () {
+   carousel();
+});
 //# sourceMappingURL=main.bundle.js.map
---
+++ src/js/main.js
@@ -0,0 +1,22 @@
+var carousel = (function(){
+  console.log("loles");
+  $('.slider-for').slick({
+   slidesToShow: 1,
+   slidesToScroll: 1,
+   arrows: false,
+   fade: true,
+   asNavFor: '.slider-nav'
+  });
+  $('.slider-nav').slick({
+     slidesToShow: 1,
+     slidesToScroll: 1,
+     asNavFor: '.slider-for',
+     dots: true,
+     centerMode: true,
+     focusOnSelect: true
+  });
+})
+
+$(document).ready(function() {
+    carousel();
+});
